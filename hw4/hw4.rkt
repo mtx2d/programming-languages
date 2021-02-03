296f59f956ff384c195d04a43aaa54b817c26185
mtx2d <matrix2d@outlook.com>
2021-02-03 04:08:33 +0000

    add test script and use map in problem 2

--- hw4/hw4.rkt
+++ hw4/hw4.rkt
@@ -3,15 +3,12 @@
 (provide (all-defined-out)) ;; so we can put tests in a second file
 
 ;; put your code below
-
+;; problem 1
 (define (sequence low high stride)
     (if (> low high) 
         null
         (cons low (sequence (+ low stride) high stride))))
 
-
+;; problem 2
 (define (string-append-map xs suffix)
-    (if (null? xs) 
-        null
-        (cons (string-append (car xs) suffix) (string-append-map (cdr xs) suffix)))
-)
+    (map (lambda (x) (string-append x suffix)) xs))
