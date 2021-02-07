207b8f4ec712d47aac955f7d79960cffc7e42333
mtx2d <matrix2d@outlook.com>
2021-02-07 23:00:00 +0000

    add files

---
+++ hw5/hw5_notes.rkt
@@ -0,0 +1,8 @@
+;; use list and symbol and helper function to create an interpreter
+;; feature, struct construct, better implementation for interpreter, 
+;; and next why struct is better than the list based interpreter
+;; struct is indeed special
+
+;; problem 1
+(define (eval-exp exp)
+)
